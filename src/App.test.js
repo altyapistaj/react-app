0bdbd12576ac239b66e85747be3fd2a60747a688
altyapistaj <altyapi_staj@proton.me>
2026-01-22 09:06:46 +0300

    App.test.js updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Hello Crazy World!/i)).toBeInTheDocument();
+});
+
+test('changes adjective when button is clicked', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /Change Adjective/i });
+
+  expect(screen.getByText(/Hello Crazy World!/i)).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  expect(screen.getByText(/Hello Beautiful World!/i)).toBeInTheDocument();
 });
